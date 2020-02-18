ab98ff8d6b8c2d6a29081c5a51c3f4657dcc6734
Artem <artem.antonov1993@gmail.com>
2020-02-19 00:20:41 +0300

    feat: add solution task 'Carbon dating'

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let decayRate = 0;
+  if (typeof sampleActivity == 'string' && parseFloat(sampleActivity)) decayRate = parseFloat(sampleActivity);
+  else return false;
+
+  if (decayRate >= 15 || decayRate <= 0) return false;
+  const constanta = 0.693;
+
+  let age = (Math.log(MODERN_ACTIVITY / decayRate)) / (constanta / HALF_LIFE_PERIOD);
+  return Math.ceil(age);
 };
